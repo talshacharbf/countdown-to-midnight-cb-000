8cafddd231ad792307f32beb864bb934fca1c29e
tal shachar <tal.shachar@buzzfeed.com>
2017-12-27 07:20:15 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  number = n
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+end
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  number = n
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(number)
+    number -= 1
+end
+"HAPPY NEW YEAR!"
 end
