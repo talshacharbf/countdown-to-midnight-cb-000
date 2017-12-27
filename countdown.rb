4fe50af79d225e972f9edba638fe3ff54462a775
tal shachar <tal.shachar@buzzfeed.com>
2017-12-27 07:18:34 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(n)
 end
 "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(n)
+  number = n
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+end
+"HAPPY NEW YEAR!"
+end
